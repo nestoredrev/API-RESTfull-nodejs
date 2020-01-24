3a8946ad1ab06a83e2ed31b2d0cf598658b71527
nestor <jokera666@gmail.com>
2020-01-24 13:48:51 +0100

    Preperring Heroku2

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 module.exports = {
 	port: process.env.PORT || 3000,
-	db: process.env.MONGODB || 'mongodb://localhost:27017/shop',
+	db: process.env.MONGODB_URI || 'mongodb://localhost:27017/shop',
 	SECRET_TOKEN: '!666satanicTOken666?'
 }
