06fd7b691773a1af5f72b60cb01423cc8dbd6c69
nestor <jokera666@gmail.com>
2020-01-24 15:52:28 +0100

    Añadir header de las peticiones

--- app.js
+++ app.js
@@ -25,6 +25,12 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())// Definier el cuerpo del mensaje con el formato JSON
 
+//Set cabezeras de las peticiones
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 //Configuracion del motor de plantillas de Handlebars para express
